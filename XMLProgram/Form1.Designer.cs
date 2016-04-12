7656d0ae1f8701610c850fac4bc6954992941e0a
Mr. Theodoropoulos <tony.theodoropoulos@ed.amdsb.ca>
2016-04-12 08:20:33 -0400

    Initial Commit

---
+++ XMLProgram/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace XMLProgram
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.readButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.writeButton = new System.Windows.Forms.Button();
+            this.nodeSelect = new System.Windows.Forms.TextBox();
+            this.contentSelect = new System.Windows.Forms.TextBox();
+            this.editButton = new System.Windows.Forms.Button();
+            this.newContent = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // readButton
+            // 
+            this.readButton.Location = new System.Drawing.Point(38, 23);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(64, 32);
+            this.readButton.TabIndex = 0;
+            this.readButton.Text = "Read File";
+            this.readButton.UseVisualStyleBackColor = true;
+            this.readButton.Click += new System.EventHandler(this.readButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(35, 122);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(305, 284);
+            this.outputLabel.TabIndex = 1;
+            // 
+            // writeButton
+            // 
+            this.writeButton.Location = new System.Drawing.Point(108, 23);
+            this.writeButton.Name = "writeButton";
+            this.writeButton.Size = new System.Drawing.Size(64, 32);
+            this.writeButton.TabIndex = 2;
+            this.writeButton.Text = "Write File";
+            this.writeButton.UseVisualStyleBackColor = true;
+            this.writeButton.Click += new System.EventHandler(this.writeButton_Click);
+            // 
+            // nodeSelect
+            // 
+            this.nodeSelect.Location = new System.Drawing.Point(220, 35);
+            this.nodeSelect.Name = "nodeSelect";
+            this.nodeSelect.Size = new System.Drawing.Size(100, 20);
+            this.nodeSelect.TabIndex = 3;
+            // 
+            // contentSelect
+            // 
+            this.contentSelect.Location = new System.Drawing.Point(220, 63);
+            this.contentSelect.Name = "contentSelect";
+            this.contentSelect.Size = new System.Drawing.Size(100, 20);
+            this.contentSelect.TabIndex = 4;
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(338, 36);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(47, 46);
+            this.editButton.TabIndex = 5;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // newContent
+            // 
+            this.newContent.Location = new System.Drawing.Point(220, 89);
+            this.newContent.Name = "newContent";
+            this.newContent.Size = new System.Drawing.Size(100, 20);
+            this.newContent.TabIndex = 6;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 448);
+            this.Controls.Add(this.newContent);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.contentSelect);
+            this.Controls.Add(this.nodeSelect);
+            this.Controls.Add(this.writeButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.readButton);
+            this.Name = "mainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button readButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button writeButton;
+        private System.Windows.Forms.TextBox nodeSelect;
+        private System.Windows.Forms.TextBox contentSelect;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.TextBox newContent;
+    }
+}
+
---
+++ XMLProgram/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XMLProgram
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void readButton_Click(object sender, EventArgs e)
+        {
+            /* READ FROM XML FILE
+            * 
+            * The following code will open an XML file and read
+            * it one line at a time, while also displaying it to
+            * the console screen
+            */
+
+            // Open the file to be read
+            XmlTextReader reader = new XmlTextReader("information.xml");
+
+            // Clear output label
+            outputLabel.Text = "";
+
+            // Continue to read each element and text until the file is done
+            while (reader.Read())  
+            {
+                // If the currently read item is text then print it to screen,
+                // otherwise the loop repeats getting the next piece of information
+                if (reader.NodeType == XmlNodeType.Text)
+                {
+                    outputLabel.Text += reader.Value + "\n";
+                }
+            }
+            // When done reading the file close it
+            reader.Close();
+
+        }
+
+        private void writeButton_Click(object sender, EventArgs e)
+        {
+            /* WRITE TO XML FILE
+            * 
+            * The following code will create a new XML file and 
+            * then create element headers and content.  Finally it 
+            * saves the info to the XML file.  If a file already exists
+            * with the same name it will be overwritten with the 
+            * information below.
+            */
+
+            XmlTextWriter writer = new XmlTextWriter("outputExample.xml", null);
+
+            //Write the "Class" element
+            writer.WriteStartElement("Class");
+
+            //Start "student" element
+            writer.WriteStartElement("student");
+
+            //Write sub-elements
+            writer.WriteElementString("name", "Chris");
+            writer.WriteElementString("address", "1313 Mockingbird Lane");
+            writer.WriteElementString("phone", "555-1313");
+            writer.WriteElementString("sex", "Female");
+
+            // end the "student" element
+            writer.WriteEndElement();
+
+            // end the "Class" element
+            writer.WriteEndElement();
+
+            //Write the XML to file and close the writer
+            writer.Close();
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            /* The following code will open an XML file and search 
+            * for a specific node.  When it finds the node it will
+            * then search for specific content. If found it will 
+            * replace that content with new info.
+            */
+
+            //open XML document
+            XmlDocument doc = new XmlDocument();
+            doc.Load("information.xml");
+
+            //create a node variable to represent an element
+            XmlNode node;
+
+            node = doc.DocumentElement;
+
+            //node1 is a child of node and node2 is a child of node1
+            //The name sub element is searched and if the text matches 'Chris'
+            //it is changed to 'Howard'
+            foreach (XmlNode node1 in node.ChildNodes)
+                foreach (XmlNode node2 in node1.ChildNodes)
+                    //if (node2.Name == "name" && node2.InnerText == "Chris")
+                    //{
+                    //    node2.InnerText = "Howard";
+                    //}
+                    if (node2.Name == nodeSelect.Text && node2.InnerText == contentSelect.Text)
+                    {
+                        node2.InnerText = newContent.Text;
+                    }
+            //save and close the document
+            doc.Save("information.xml");
+
+            readButton_Click(sender, e);
+        }
+    }
+}
